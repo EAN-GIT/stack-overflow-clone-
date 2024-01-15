bc25d241c8daaf8463d1f2325a4a50aafdc243e6
EA <emmanueljoeemie@gmail.com>
2024-01-15 11:49:35 +0100

    added tag functionality

--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -3,7 +3,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm, useFormContext } from "react-hook-form";
 import * as z from "zod";
-import React from "react";
+import React, { ReactEventHandler } from "react";
 import { Editor } from "@tinymce/tinymce-react";
 
 // Import dotenv to load environment variables from a .env file
@@ -24,6 +24,8 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { QuestionsSchema } from "@/lib/validation";
+import { Badge } from "../ui/badge";
+import Image from "next/image";
 
 const QuestionForm = () => {
   // 1. Define your form.
@@ -42,6 +44,57 @@ const QuestionForm = () => {
     // ✅ This will be type-safe and validated.
     console.log(values);
   }
+
+//handle for tags on Enter press
+ function handleInputKeyDown(e:React.KeyboardEvent<HTMLInputElement>,field:any){
+
+    if(e.key === "Enter" && field.name === 'tags' ){
+        e.preventDefault();
+      
+      
+        //gettingthe input
+        const tagInput = e.target as HTMLInputElement;
+        const tagValue = tagInput.value.trim(); 
+
+        if(tagValue !== ''){
+            if(tagValue.length > 15){
+
+             return form.setError('tags',{
+                type: "required",
+                message:"Tag must be less than 15 characters."
+             })   
+
+            }
+            //ensure we dont add duplicat tags
+
+            if(!field.value.includes(tagValue as never)){
+                form.setValue('tags',[...field.value,tagValue])
+                tagInput.value = ''
+                form.clearErrors('tags');
+            }else{
+                form.trigger();
+            }
+        }
+
+    }
+
+
+
+
+}
+
+
+    // delete tag functionality
+
+    function handleTagRemove(tag:string,field:any){
+
+        const newTags = field.value.filter((t:string)=> t !== tag)
+
+        form.setValue('tags', newTags)
+
+
+    }
+
   return (
     <Form {...form}>
       <form
@@ -85,7 +138,7 @@ const QuestionForm = () => {
               <FormControl className="mt-3.5  background-light900_dark300">
                 {/* add a Todo and editor comp ===TINY MCE*/}
                 <Editor  //work on the styling darkmode and env====================================\\\\\\\\\\\\\
-                  apiKey=""
+                  apiKey="ecnx6s6i8wporc6dj3jl8ermzcqpojvg9g6iei7jh34ymi3w"
                 //   {process.env.TINYMCE_EDITOR_API_KEY}
                   init={{
                     plugins:
@@ -118,24 +171,53 @@ const QuestionForm = () => {
           )}
         />
         {/* Tag field  */}
+      
         <FormField
           control={form.control}
           name="tags"
           render={({ field }) => (
             <FormItem className="flex w-full flex-col">
-              <FormLabel className="paragraph-semibold text-dark400_light800 ">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
                 Tags <span className="text-primary-500">*</span>
               </FormLabel>
               <FormControl className="mt-3.5">
-                <Input
-                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px border"
-                  {...field}
-                  placeholder="Add tags..."
-                />
+                <>
+                  <Input
+                    // disabled={type === "Edit"}
+                    className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
+                    placeholder="Add tags..."
+                    onKeyDown={(e) => handleInputKeyDown(e, field)}
+                  />
+
+                  {field.value.length > 0 && (
+                    <div className="flex-start mt-2.5 gap-2.5">
+                      {field.value.map((tag: any) => (
+                        <Badge
+                          key={tag}
+                          className="subtle-medium background-light800_dark300 text-light400_light500 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize"
+                          onClick={() =>
+                               handleTagRemove(tag, field)
+                          }
+                        >
+                          {tag}
+                          {
+                            <Image
+                              src="/assets/icons/close.svg"
+                              alt="Close icon"
+                              width={12}
+                              height={12}
+                              className="cursor-pointer object-contain invert-0 dark:invert"
+                            />
+                          }
+                        </Badge>
+                      ))}
+                    </div>
+                  )}
+                </>
               </FormControl>
-              <FormDescription className="body-regulat mt-2.5 text-light-500">
-                Add up t0 3 tags to describe what your question is about.You
-                need to press enter to add a tag
+              <FormDescription className="body-regular mt-2.5 text-light-500">
+                Add up to 3 tags to describe what your question is about. You
+                need to press enter to add a tag.
               </FormDescription>
               <FormMessage className="text-red-500" />
             </FormItem>
