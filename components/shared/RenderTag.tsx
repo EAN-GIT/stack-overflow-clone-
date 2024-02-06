ad0ca625d88d0220ee5ac6dfd5423b54691854f0
EAN-GIT <emmanueljoeemie@gmail.com>
2024-02-06 17:40:13 +0100

    added func for dynamic popular tags

--- components/shared/RenderTag.tsx
+++ components/shared/RenderTag.tsx
@@ -6,11 +6,11 @@ interface Props {
   key: string;
   _id: string;
   name: string;
-  totalCount?: string;
+  totalQuestions?: string;
   showCount?: boolean;
 }
 
-const RenderTag = ({ _id, name, totalCount, showCount }: Props) => {
+const RenderTag = ({ _id, name, totalQuestions, showCount }: Props) => {
   return (
     <Link href={`./tags/${_id}`} className="flex justify-between gap-2">
       <Badge className="text-light400_light500 subtle-medium rounded-md border-none bg-light-800 px-4 py-2 uppercase dark:bg-dark-400">
@@ -18,7 +18,7 @@ const RenderTag = ({ _id, name, totalCount, showCount }: Props) => {
       </Badge>
 
       {showCount && (
-        <p className="small-medium text-dark500_light700">{totalCount}</p>
+        <p className="small-medium text-dark500_light700">{totalQuestions}</p>
       )}
     </Link>
   );
--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -3,9 +3,11 @@ import Link from "next/link";
 import React from "react";
 import RenderTag from "./RenderTag";
 import { getHotQuestions } from "@/lib/actions/question.action";
+import { getTopPopularTags } from "@/lib/actions/tag.action";
 
 const RightSidebar = async () => {
   const hotQuestions = await getHotQuestions();
+  const popularTags = await getTopPopularTags();
 
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
